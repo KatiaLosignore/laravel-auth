5ee6b46531e27c22cd70f99cccaa5345a829fac4
KatiaLosignore <katialos.kl@gmail.com>
2023-05-22 19:26:44 +0200

    added migrations

--- database/migrations/2023_05_22_165157_create_projects_table.php
+++ database/migrations/2023_05_22_165157_create_projects_table.php
@@ -15,6 +15,11 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 150)->unique();
+            $table->text('content')->nullable();
+            $table->string('image')->nullable();
+            $table->string('link_project')->unique();
+            $table->string('slug')->unique();
             $table->timestamps();
         });
     }
