40f2ca36ac11a0ffc4e16bfa5ba3251e654f2f4f
KatiaLosignore <katialos.kl@gmail.com>
2023-05-22 19:51:13 +0200

    added  seeders

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -2,8 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+use Faker\Generator as Faker;
 
 class ProjectSeeder extends Seeder
 {
@@ -12,8 +16,18 @@ class ProjectSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i=0; $i<10; $i++) {
+
+            $project = new Project();
+
+            $project->title = $faker->sentence(4);
+            $project->content = $faker->text(500);
+            $project->image = $faker->imageUrl(200, 200);
+            $project->link_project = $faker->url();
+            $project->slug = Str::slug($project->title, '-');
+            $project->save();
+        }
     }
 }
