8e1c8be604222ba4938b16756fbb97cb48ad9ac3
KatiaLosignore <katialos.kl@gmail.com>
2023-05-23 20:32:30 +0200

    added delete with confirm

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -25,10 +25,34 @@
                     <a class="btn btn-primary me-2" href="{{route('admin.projects.show', $project->slug)}}">Detail</a>
                     <a class="btn btn-warning me-2" href="{{route('admin.projects.edit', $project->slug)}}">Edit</a>
 
+                    <form class="form_delete_project" action="{{route('admin.projects.destroy', ['project' => $project->slug])}}" method="POST">
+                      @csrf
+                      @method('DELETE')
+                      <button type="submit" class="btn btn-danger">Elimina</button>
+                  </form>
+
                 </td>
             </tr>
         @endforeach
     </tbody>
-  </table>
+</table>
+
+<div class="modal fade" id="confirmModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+      <div class="modal-content">
+      <div class="modal-header">
+          <h1 class="modal-title fs-5" id="exampleModalLabel">Conferma eliminazione</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+          Confermi di voler eliminare l'elemento selezionato?
+      </div>
+      <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-danger">Conferma eliminazione</button>
+      </div>
+      </div>
+  </div>
+</div>
 
 @endsection
